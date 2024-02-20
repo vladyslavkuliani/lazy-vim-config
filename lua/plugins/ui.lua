ad9c453ffd9a1d8a8b3e2c34224b13ca24230c8d
vladyslavkuliani <vladyslav.k@hotmail.com>
2024-02-19 22:06:43 -0800

    lazy-vim init and tweaks

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,23 @@
+return {
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        -- globalstatus = false,
+        theme = "solarized-osaka",
+      },
+    },
+  },
+  -- animations
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
+}
